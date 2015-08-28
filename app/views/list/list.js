af8aad62a7cd27b6322d068fc56536340036d05f
TJ VanToll <tj.vantoll@gmail.com>
2015-08-28 09:55:52 -0400

    Implement an activity indicator for the list page
    Fixes #1

--- app/views/list/list.js
+++ app/views/list/list.js
@@ -23,7 +23,10 @@ exports.loaded = function(args) {
 	}
 	page.bindingContext = pageData;
 	groceryList.empty();
-	groceryList.load();
+	pageData.set("isLoading", true);
+	groceryList.load().then(function() {
+		pageData.set("isLoading", false);
+	});
 };
 
 exports.add = function() {
