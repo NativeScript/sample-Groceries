5be613e9c9494fd103b73621fcde1c665939f863
TJ VanToll <tj.vantoll@gmail.com>
2015-09-30 15:28:34 -0400

    Linting fix

--- app/shared/utils/form-util.js
+++ app/shared/utils/form-util.js
@@ -4,6 +4,6 @@ exports.hideKeyboardOnBlur = function(page, views) {
 	page.observe(gesturesModule.GestureTypes.tap, function(view) {
 		views.forEach(function(view) {
 			view.dismissSoftInput();
-		})
+		});
 	});
 };
