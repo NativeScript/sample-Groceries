15730a6db3e8e2cd2217a0316f37cf24cb089853
TJ VanToll <tj.vantoll@gmail.com>
2015-12-23 11:44:10 -0500

    Constants ftw!

--- app/shared/utils/action-bar-util.js
+++ app/shared/utils/action-bar-util.js
@@ -4,7 +4,8 @@ exports.styleActionBar = function() {
 	var topmost = frameModule.topmost();
 	if (topmost.ios) {
 		// Make the iOS status bar use white text
+		// Yes, UIBarStyleBlack = white text, because logic
 		var navigationBar = topmost.ios.controller.navigationBar;
-		navigationBar.barStyle = 1;
+		navigationBar.barStyle = UIBarStyle.UIBarStyleBlack;
 	}
 };
