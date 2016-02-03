adac67da9b829365cf6dd9364d811afd78d4f69c
TJ VanToll <tj.vantoll@gmail.com>
2016-02-03 14:06:39 -0500

    Matching Angular 2’s conventions

--- app/main.ts
+++ app/main.ts
@@ -2,6 +2,6 @@ import {nativeScriptBootstrap} from "nativescript-angular/application";
 import {provide} from "angular2/core";
 import {ROUTER_PROVIDERS, LocationStrategy} from "angular2/router";
 import {NSLocationStrategy} from "./ns-location-strategy";
-import {AppComponent} from "./app-component";
+import {AppComponent} from "./app.component";
 
 nativeScriptBootstrap(AppComponent, [ROUTER_PROVIDERS, provide(LocationStrategy, { useClass: NSLocationStrategy })]);
