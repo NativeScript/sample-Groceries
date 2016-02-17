21a7d97d14080be92adafe6396cec262a35fccf3
Hristo Deshev <hristo@deshev.com>
2016-02-17 13:22:05 +0200

    Fix tsconfig issues. Not listing each file anymore.

---
+++ app/references.d.ts
@@ -0,0 +1,3 @@
+/// <reference path="../node_modules/angular2/typings/es6-shim/es6-shim.d.ts" />
+/// <reference path="../node_modules/angular2/typings/node/node.d.ts" />
+/// <reference path="../node_modules/tns-core-modules/tns-core-modules.base.d.ts" />
