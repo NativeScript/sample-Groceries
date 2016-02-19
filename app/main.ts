4404716490b74b7d703158dc287654c21d1519fe
Hristo Deshev <hristo@deshev.com>
2016-02-19 15:54:08 +0200

    Patch livesync to avoid crashes on "page reload" syncs.

--- app/main.ts
+++ app/main.ts
@@ -2,5 +2,6 @@ import {nativeScriptBootstrap} from "nativescript-angular/application";
 import {HTTP_PROVIDERS} from "angular2/http";
 import {NS_ROUTER_PROVIDERS} from "nativescript-angular/router";
 import {AppComponent} from "./app.component";
+import "./livesync-patch";
 
 nativeScriptBootstrap(AppComponent, [HTTP_PROVIDERS, NS_ROUTER_PROVIDERS]);
