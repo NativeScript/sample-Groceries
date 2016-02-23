a271f5893510264ffe95f4360614038c5c8e82b8
TJ VanToll <tj.vantoll@gmail.com>
2016-02-23 09:57:25 -0500

    Upgrade to {N} 1.6.2 and remove Android workaround

--- app/app.js
+++ app/app.js
@@ -2,7 +2,5 @@ var applicationModule = require("application");
 var navigation = require("./shared/navigation");
 
 applicationModule.start({
-	// Workaround for https://github.com/NativeScript/NativeScript/issues/1577
-	// After 1.7 always pass navigation.startingPage()
-	moduleName: global.android? "views/login/login" : navigation.startingPage()
+	moduleName: navigation.startingPage()
 });
