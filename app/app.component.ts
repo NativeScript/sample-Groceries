fe675c434d7ee92816107734a446b273ee63154e
TJ VanToll <tj.vantoll@gmail.com>
2016-03-07 13:48:31 -0500

    Use consistent indentation

--- app/app.component.ts
+++ app/app.component.ts
@@ -7,13 +7,13 @@ import {RegisterPage} from "./pages/register/register.component";
 import {ListPage} from "./pages/list/list.component";
 
 @Component({
-    selector: "main",
-    directives: [NS_ROUTER_DIRECTIVES],
-    template: "<StackLayout><page-router-outlet></page-router-outlet></StackLayout>"
+  selector: "main",
+  directives: [NS_ROUTER_DIRECTIVES],
+  template: "<StackLayout><page-router-outlet></page-router-outlet></StackLayout>"
 })
 @RouteConfig([
-    { path: "/", component: LoginPage, as: "Login" },
-    { path: "/Register", component: RegisterPage, as: "Register" },
-    { path: "/List", component: ListPage, as: "List" }
+  { path: "/", component: LoginPage, as: "Login" },
+  { path: "/Register", component: RegisterPage, as: "Register" },
+  { path: "/List", component: ListPage, as: "List" }
 ])
 export class AppComponent {}
