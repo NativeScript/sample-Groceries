3690853e3e452aaeb8fb373f6118dc5cd925c641
TJ VanToll <tj.vantoll@gmail.com>
2016-03-10 11:39:50 -0500

    Starting on the iOS implementation

--- app/app.js
+++ app/app.js
@@ -1,6 +1,18 @@
 var applicationModule = require("application");
 var navigation = require("./shared/navigation");
 
+var AppDelegate = UIResponder.extend({
+	applicationDidFinishLaunchingWithOptions: function () {
+		// Set the status bar to light content
+		UIApplication.sharedApplication().statusBarStyle = UIStatusBarStyle.LightContent;
+		return true;
+	}
+}, {
+	name: "AppDelegate",
+	protocols: [UIApplicationDelegate]
+});
+applicationModule.ios.delegate = AppDelegate;
+
 applicationModule.start({
 	moduleName: navigation.startingPage()
 });
