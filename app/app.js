bb37c567e25ff1f49b176a7ddca9c869673b3021
TJ VanToll <tj.vantoll@gmail.com>
2016-03-10 11:51:36 -0500

    Setup platform-specific CSS files for Android

--- app/app.js
+++ app/app.js
@@ -1,18 +1,20 @@
 var applicationModule = require("application");
 var navigation = require("./shared/navigation");
 
-var AppDelegate = UIResponder.extend({
-	applicationDidFinishLaunchingWithOptions: function () {
-		// TODO: Switch to UIStatusBarStyle.LightContent after figuring out where
-		// that white background is coming from
-		UIApplication.sharedApplication().statusBarStyle = UIStatusBarStyle.Default;
-		return true;
-	}
-}, {
-	name: "AppDelegate",
-	protocols: [UIApplicationDelegate]
-});
-applicationModule.ios.delegate = AppDelegate;
+if (applicationModule.ios) {
+  var AppDelegate = UIResponder.extend({
+  	applicationDidFinishLaunchingWithOptions: function () {
+  		// TODO: Switch to UIStatusBarStyle.LightContent after figuring out where
+  		// that white background is coming from
+  		UIApplication.sharedApplication().statusBarStyle = UIStatusBarStyle.Default;
+  		return true;
+  	}
+  }, {
+  	name: "AppDelegate",
+  	protocols: [UIApplicationDelegate]
+  });
+  applicationModule.ios.delegate = AppDelegate;
+}
 
 applicationModule.start({
 	moduleName: navigation.startingPage()
