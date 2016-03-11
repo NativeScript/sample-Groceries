6eaf0050aa5b3ddc7c8b8b3bbcf41d06d9ad0ef8
TJ VanToll <tj.vantoll@gmail.com>
2016-03-11 12:54:50 -0500

    Finally fixing the status bar for iOS

--- app/app.js
+++ app/app.js
@@ -2,18 +2,16 @@ var applicationModule = require("application");
 var navigation = require("./shared/navigation");
 
 if (applicationModule.ios) {
-  var AppDelegate = UIResponder.extend({
-  	applicationDidFinishLaunchingWithOptions: function () {
-  		// TODO: Switch to UIStatusBarStyle.LightContent after figuring out where
-  		// that white background is coming from
-  		UIApplication.sharedApplication().statusBarStyle = UIStatusBarStyle.Default;
-  		return true;
-  	}
-  }, {
-  	name: "AppDelegate",
-  	protocols: [UIApplicationDelegate]
-  });
-  applicationModule.ios.delegate = AppDelegate;
+	var AppDelegate = UIResponder.extend({
+		applicationDidFinishLaunchingWithOptions: function () {
+			UIApplication.sharedApplication().statusBarStyle = UIStatusBarStyle.LightContent;
+			return true;
+		}
+	}, {
+		name: "AppDelegate",
+		protocols: [UIApplicationDelegate]
+	});
+	applicationModule.ios.delegate = AppDelegate;
 }
 
 applicationModule.start({
