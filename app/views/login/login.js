7c89a29bbb0dcb36a95e025c7cd41f5ddb9cfa29
TJ VanToll <tj.vantoll@gmail.com>
2016-03-14 11:31:08 -0400

    Final background images

--- app/views/login/login.js
+++ app/views/login/login.js
@@ -1,3 +1,4 @@
+var animationModule = require("ui/animation");
 var dialogsModule = require("ui/dialogs");
 var Observable = require("data/observable").Observable;
 
@@ -41,11 +42,37 @@ exports.loaded = function(args) {
 		container.android.setFocusableInTouchMode(true);
 		container.android.setFocusable(true);
 		email.android.clearFocus();
+
+		var initialLabel = page.getViewById("initial-label");
+		initialLabel.android.setLetterSpacing(0.3);
 	}
 
-	// Add a parallax effect to the background
+	var definitions = [];
+
+	// Parallax background
 	var background = page.getViewById("background");
-	background.animate({ scale: { x: 1, y: 1 }, duration: 8000 });
+	definitions.push({ target: background, scale: { x: 1, y: 1 }, duration: 8000 });
+
+	// Pear
+	// definitions.push({ target: pear, opacity: 1, delay: 500, duration: 500 });
+	// definitions.push({ target: pear, rotate: 360, delay: 750, duration: 2000 });
+
+	// Apple
+	// definitions.push({ target: apple, opacity: 1, delay: 1000, duration: 1000 });
+	// definitions.push({ target: apple, rotate: 360, delay: 750, duration: 2000 });
+
+	// Banana
+	// definitions.push({ target: banana, opacity: 1, delay: 1000, duration: 500 });
+	// definitions.push({ target: banana, scale: { x: 1.6, y: 1.6 }, delay: 1500, duration: 500 });
+	// definitions.push({ target: banana, scale: { x: 1, y: 1 }, delay: 2000, duration: 500 });
+	// definitions.push({ target: banana, scale: { x: 1.6, y: 1.6 }, delay: 2500, duration: 500 });
+	// definitions.push({ target: banana, scale: { x: 1, y: 1 }, delay: 3000, duration: 500 });
+
+	new animationModule.Animation(definitions, false)
+		.play()
+		.catch(function (e) {
+			console.log(e.message);
+		});
 };
 
 exports.focusPassword = function() {
