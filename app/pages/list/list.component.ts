44d789016e32482d96222705ad6aedf565b936b2
TJ VanToll <tj.vantoll@gmail.com>
2016-03-16 12:21:15 -0400

    Greatly simplifying the ListView syntax!

--- app/pages/list/list.component.ts
+++ app/pages/list/list.component.ts
@@ -1,12 +1,9 @@
 import {Component, ChangeDetectionStrategy, OnInit} from "angular2/core";
 import {Router} from "angular2/router";
-import {Observable} from "rxjs/Observable";
 
 import * as dialogsModule from "ui/dialogs";
-import {ActionItem} from "ui/action-bar";
 import {TextField} from "ui/text-field";
 import {topmost} from "ui/frame";
-import {WrappedValue} from "data/observable";
 
 import {Grocery} from "../../shared/grocery/grocery";
 import {GroceryListService} from "../../shared/grocery/grocery-list.service";
@@ -14,37 +11,27 @@ import {GroceryListService} from "../../shared/grocery/grocery-list.service";
 @Component({
   selector: "list",
   templateUrl: "pages/list/list.html",
-  // TODO: Why is this necessary?
-  changeDetection: ChangeDetectionStrategy.OnPush,
   providers: [GroceryListService]
 })
 export class ListPage implements OnInit {
-  groceryList: Observable<Array<Grocery>>;
+  groceryList: Array<Grocery> = [];
   grocery: string;
   isLoading: boolean;
 
-  private subscr;
-  private items: Array<Grocery> = [];
-
   constructor(
     private _groceryListService: GroceryListService,
-    private router: Router) {
+    private _router: Router) {
 
     this.grocery = "";
     this.isLoading = true;
-
-    // TODO: I have no idea what’s going on here
-    this.groceryList = Observable.create(subscriber => {
-      this.subscr = subscriber;
-      subscriber.next(this.items);
-    });
   }
 
   ngOnInit() {
     this._groceryListService.load()
       .subscribe(loadedGroceries => {
-        this.items = loadedGroceries;
-        this.updateList();
+        loadedGroceries.forEach((groceryObject) => {
+          this.groceryList.unshift(groceryObject);
+        });
       });
   }
 
@@ -65,8 +52,7 @@ export class ListPage implements OnInit {
     this._groceryListService.add(this.grocery)
       .subscribe(
         groceryObject => {
-          this.items.push(groceryObject);
-          this.updateList();
+          this.groceryList.unshift(groceryObject);
           this.grocery = "";
         },
         () => {
@@ -78,16 +64,4 @@ export class ListPage implements OnInit {
         }
       )
   }
-
-  updateList() {
-    this.subscr.next(this.items);
-  }
-
-  //delete(grocery) {
-    //this._groceryListService.delete(grocery.id)
-      //.subscribe(() => {
-        //var index = this.groceryList.indexOf(grocery);
-        //this.groceryList.splice(index, 1);
-      //});
-  //}
 }
