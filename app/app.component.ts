26946f55649282c5b177957a98f972ee31a83f69
TJ VanToll <tj.vantoll@gmail.com>
2016-03-18 11:14:13 -0400

    Working around the iOS ActionItem issue

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,12 +1,14 @@
 import {Component} from "angular2/core";
 import {RouteConfig} from "angular2/router";
 import {NS_ROUTER_DIRECTIVES} from "nativescript-angular/router";
+import * as application from "application";
 
 import * as Config from "./shared/config";
 import {LoginPage} from "./pages/login/login.component";
 import {ListPage} from "./pages/list/list.component";
 
-var startOnList = !!Config.token;
+// Workaround https://github.com/NativeScript/nativescript-angular/issues/121
+var startOnList = (!!Config.token) && (!!application.android);
 
 @Component({
   selector: "main",
