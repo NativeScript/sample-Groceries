6b519936239da5a8796523c7f724f38fd7111cd7
TJ VanToll <tj.vantoll@gmail.com>
2016-03-29 10:35:15 -0400

    Removing LiveSync patch

--- app/main.ts
+++ app/main.ts
@@ -1,7 +1,4 @@
 import {nativeScriptBootstrap} from "nativescript-angular/application";
 import {AppComponent} from "./app.component";
 
-// TODO: This is temporary. Remove after this is fixed.
-import "./livesync-patch";
-
 nativeScriptBootstrap(AppComponent);
