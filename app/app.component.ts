217e07ffabe4f92fbed267f08aed33927c16a402
TJ VanToll <tj.vantoll@gmail.com>
2016-04-11 15:39:13 -0400

    Revert "Changing starting CSS state"
    This reverts commit d6ab6a6d078998e33b4f3e980de6c02697584c77.

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,18 +1,7 @@
 import {Component} from "angular2/core";
-import {HTTP_PROVIDERS} from "angular2/http";
-import {RouteConfig} from "angular2/router";
-import {NS_ROUTER_DIRECTIVES, NS_ROUTER_PROVIDERS} from "nativescript-angular/router";
-import {LoginPage} from "./pages/login/login.component";
-import {ListPage} from "./pages/list/list.component";
 
 @Component({
-  selector: "main",
-  directives: [NS_ROUTER_DIRECTIVES],
-  providers: [HTTP_PROVIDERS, NS_ROUTER_PROVIDERS],
-  template: "<page-router-outlet></page-router-outlet>"
+  selector: "my-app",
+  template: "<Label text='hello world'></Label>"
 })
-@RouteConfig([
-  { path: "/Login", component: LoginPage, as: "Login", useAsDefault: true },
-  { path: "/List", component: ListPage, as: "List" }
-])
 export class AppComponent {}
