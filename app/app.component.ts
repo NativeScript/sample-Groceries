0a44087c47cb005107fcf0d3071953fb55a7fdd9
TJ VanToll <tj.vantoll@gmail.com>
2016-05-07 13:00:34 -0400

    Revert "Revert "Revert "Upgrading to Angular RC 1"""
    This reverts commit 08f43f8843e5db421102230eb03107ac7de92e60.

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,6 +1,6 @@
-import {Component} from "@angular/core";
-import {HTTP_PROVIDERS} from "@angular/http";
-import {RouteConfig} from "@angular/router-deprecated";
+import {Component} from "angular2/core";
+import {HTTP_PROVIDERS} from "angular2/http";
+import {RouteConfig} from "angular2/router";
 import {NS_ROUTER_DIRECTIVES, NS_ROUTER_PROVIDERS} from "nativescript-angular/router";
 import {LoginPage} from "./pages/login/login.component";
 import {ListPage} from "./pages/list/list.component";
