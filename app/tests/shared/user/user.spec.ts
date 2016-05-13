8ec9f592b9fe803259e37a20f3a51dbc24ca6bf7
TJ VanToll <tj.vantoll@gmail.com>
2016-05-13 16:26:21 -0400

    Unit tests! 🏆

---
+++ app/tests/shared/user/user.spec.ts
@@ -0,0 +1,30 @@
+import {User} from "../../../shared/user/user";
+
+declare var describe: any;
+declare var expect: any;
+declare var it: any;
+
+describe("Email validation", function() {
+  let user = new User();
+
+  it("Should reject an empty email address", function () {
+    user.email = "";
+    expect(user.isValidEmail()).toBe(false);
+  });
+
+  it("Should reject a malformed email addresses", function() {
+    user.email = "nativescript";
+    expect(user.isValidEmail()).toBe(false);
+
+    user.email = "nativescript@";
+    expect(user.isValidEmail()).toBe(false);
+
+    user.email = "nativescript@isawesome";
+    expect(user.isValidEmail()).toBe(false);
+  });
+
+  it("Should accept valid email addresses", function() {
+    user.email = "nativescript@isawesome.com";
+    expect(user.isValidEmail()).toBe(true);
+  });
+});
