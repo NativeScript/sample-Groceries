a7340c241780edbb3e4bd69ba835728dcacb7d1c
TJ VanToll <tj.vantoll@gmail.com>
2016-06-02 14:11:21 -0400

    Remove some files no longer used on this branch 🔪☠️

--- app/shared/utils/form-util.js
+++
@@ -1,9 +0,0 @@
-var gesturesModule = require("ui/gestures");
-
-exports.hideKeyboardOnBlur = function(page, views) {
-	page.observe(gesturesModule.GestureTypes.tap, function() {
-		views.forEach(function(view) {
-			view.dismissSoftInput();
-		});
-	});
-};
