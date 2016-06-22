abac80a28c3ece3e9904e0e65fc3168a0e4d0929
vakrilov <alexander.vakrilov@telerik.com>
2016-06-22 15:27:11 +0300

    fixed 'already navigating' crash

--- app/app.component.ts
+++ app/app.component.ts
@@ -5,7 +5,6 @@ import {NS_ROUTER_DIRECTIVES, NS_ROUTER_PROVIDERS} from "nativescript-angular/ro
 @Component({
   selector: "main",
   directives: [NS_ROUTER_DIRECTIVES],
-  providers: [NS_ROUTER_PROVIDERS],
   template: "<page-router-outlet></page-router-outlet>"
 })
 export class AppComponent {}
