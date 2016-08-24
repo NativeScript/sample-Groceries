d6368962e7a22ee2326f1451644d6827ea9b7016
TJ VanToll <tj.vantoll@gmail.com>
2016-08-24 10:18:36 -0400

    Updating to {N} 2.2 and Angular 2 RC4 🎉

--- app/main.ts
+++ app/main.ts
@@ -1,8 +1,8 @@
 import {nativeScriptBootstrap} from "nativescript-angular/application";
-import {HTTP_PROVIDERS} from "@angular/http";
+import {NS_HTTP_PROVIDERS} from "nativescript-angular/http";
 import {AppComponent} from "./app.component";
 import {APP_ROUTER_PROVIDERS} from "./app.routes";
 import {setStatusBarColors} from "./utils/status-bar-util";
 
 setStatusBarColors();
-nativeScriptBootstrap(AppComponent, [HTTP_PROVIDERS, APP_ROUTER_PROVIDERS]);
+nativeScriptBootstrap(AppComponent, [NS_HTTP_PROVIDERS, APP_ROUTER_PROVIDERS]);
