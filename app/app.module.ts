5e8c4570c429cc835107439ca9cae71fd69f9554
TJ VanToll <tj.vantoll@gmail.com>
2016-09-16 15:14:19 -0400

    Minor formatting tweaks to sync up with the tutorial

--- app/app.module.ts
+++ app/app.module.ts
@@ -4,12 +4,8 @@ import { NativeScriptModule } from "nativescript-angular/platform";
 import { AppComponent } from "./app.component";
 
 @NgModule({
-  imports: [
-    NativeScriptModule
-  ],
-  declarations: [
-    AppComponent
-  ],
+  imports: [NativeScriptModule],
+  declarations: [AppComponent],
   bootstrap: [AppComponent]
 })
 export class AppModule {}
--- app/main.ts
+++ app/main.ts
@@ -1,4 +1,5 @@
 import { platformNativeScriptDynamic } from "nativescript-angular/platform";
-import { AppModule } from './app.module';
+
+import { AppModule } from "./app.module";
 
 platformNativeScriptDynamic().bootstrapModule(AppModule);
