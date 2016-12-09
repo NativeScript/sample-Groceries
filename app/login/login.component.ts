e4a1ab53593a3086a74e03569d2a2acecd09b4cc
TJ VanToll <tj.vantoll@gmail.com>
2016-12-09 10:32:26 -0500

    Remove hardcoded username and password

--- app/login/login.component.ts
+++ app/login/login.component.ts
@@ -32,8 +32,6 @@ export class LoginComponent implements OnInit {
     private userService: LoginService,
     private page: Page) {
     this.user = new User();
-    this.user.email = "user@nativescript.org";
-    this.user.password = "password";
   }
 
   ngOnInit() {
