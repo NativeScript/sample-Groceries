e6e5534a979f551ac7dae2a6c890ac6c5868a435
TJ VanToll <tj.vantoll@gmail.com>
2016-12-09 10:51:24 -0500

    Upping tns-core-modules to 2.4.2

--- app/pages/login/login.component.ts
+++ app/pages/login/login.component.ts
@@ -25,8 +25,6 @@ export class LoginComponent implements OnInit {
 
   constructor(private router: Router, private userService: UserService, private page: Page) {
     this.user = new User();
-    this.user.email = "user@nativescript.org";
-    this.user.password = "password";
   }
 
   ngOnInit() {
