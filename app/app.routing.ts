922a914ba80d4c94af2086024afa9ce2146afd11
Stanimira Vlaeva <stanimira.vlaeva@gmail.com>
2017-01-12 13:50:25 +0200

    Revert "feat: make Login and Groceries modules lazy loaded" (#204)

--- app/app.routing.ts
+++ app/app.routing.ts
@@ -5,7 +5,5 @@ export const authProviders = [
 ];
 
 export const appRoutes = [
-  { path: "", redirectTo: "/groceries", pathMatch: "full" },
-  { path: "groceries", loadChildren: "./groceries/groceries.module#GroceriesModule" },
-  { path: "login", loadChildren: "./login/login.module#LoginModule" },
+  { path: "", redirectTo: "/groceries", pathMatch: "full" }
 ];
--- app/login/login.routing.ts
+++ app/login/login.routing.ts
@@ -4,6 +4,6 @@ import { Routes, RouterModule } from "@angular/router";
 import { LoginComponent } from "./login.component";
 
 const loginRoutes: Routes = [
-  { path: "", component: LoginComponent },
+  { path: "login", component: LoginComponent },
 ];
 export const loginRouting: ModuleWithProviders = RouterModule.forChild(loginRoutes);
