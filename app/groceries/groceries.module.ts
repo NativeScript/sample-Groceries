d026d739d733904f098c7f22ca8066b35f1a64f1
Hristo Deshev <hristo@deshev.com>
2017-02-01 15:56:37 +0200

    fix(AoT): Prevent from bundling @angular/compiler

--- app/groceries/groceries.module.ts
+++ app/groceries/groceries.module.ts
@@ -1,4 +1,4 @@
-import { NativeScriptModule } from "nativescript-angular/platform";
+import { NativeScriptModule } from "nativescript-angular/nativescript.module";
 import { NativeScriptFormsModule } from "nativescript-angular/forms";
 import { NgModule, NO_ERRORS_SCHEMA } from "@angular/core";
 import { groceriesRouting } from "./groceries.routing";
