6aa2e3b2b589802d2d850d35c76c0e8e7374c4ee
Hristo Deshev <hristo@deshev.com>
2017-02-22 12:06:14 +0200

    Don't use obsolete application API.
    Switch to the "activityStarted" event.

--- app/shared/status-bar-util.ts
+++ app/shared/status-bar-util.ts
@@ -17,20 +17,18 @@ export function setStatusBarColors() {
   // Make the Android status bar transparent.
   // See http://bradmartin.net/2016/03/10/fullscreen-and-navigation-bar-color-in-a-nativescript-android-app/
   // for details on the technique used.
-  if (application.android) {
-    application.android.onActivityStarted = function() {
-      if (application.android && platform.device.sdkVersion >= "21") {
-        const View = android.view.View;
-        const window = application.android.startActivity.getWindow();
-        window.setStatusBarColor(0x000000);
+  if (application.android && platform.device.sdkVersion >= "21") {
+    application.android.on("activityStarted", () => {
+      const View = android.view.View;
+      const window = application.android.startActivity.getWindow();
+      window.setStatusBarColor(0x000000);
 
-        const decorView = window.getDecorView();
-        decorView.setSystemUiVisibility(
-          View.SYSTEM_UI_FLAG_LAYOUT_STABLE
-          | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
-          | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
-          | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY);
-      }
-    };
+      const decorView = window.getDecorView();
+      decorView.setSystemUiVisibility(
+        View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+        | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+        | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+        | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY);
+    });
   }
 }
