777ca9e62edc55adf6f8b2ab16831e285aab81c7
sis0k0 <stanimira.vlaeva@gmail.com>
2017-06-20 14:13:48 +0300

    refactor: stop importing from rxjs/Rx
    Instead import only what's needed. Importing from rxjs/Rx breaks
    tree-shaking and causes the whole rxjs library to be included in the
    bundle.

--- app/shared/login.service.ts
+++ app/shared/login.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from "@angular/core";
 import { Http, Headers, Response } from "@angular/http";
-import { Observable } from "rxjs/Rx";
+import { Observable } from "rxjs/Observable";
 import "rxjs/add/operator/do";
 import "rxjs/add/operator/map";
 import "rxjs/add/observable/throw";
