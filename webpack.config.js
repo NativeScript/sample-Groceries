60348942a4c7a9236eaf553155f572298728f6c2
sis0k0 <stanimira.vlaeva@gmail.com>
2017-06-20 14:30:23 +0300

    refactor: add bundle analyzer to webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -5,6 +5,7 @@ const nsWebpack = require("nativescript-dev-webpack");
 const nativescriptTarget = require("nativescript-dev-webpack/nativescript-target");
 const CopyWebpackPlugin = require("copy-webpack-plugin");
 const ExtractTextPlugin = require("extract-text-webpack-plugin");
+const { BundleAnalyzerPlugin } = require("webpack-bundle-analyzer");
 
 const { AotPlugin } = require("@ngtools/webpack");
 
@@ -138,6 +139,14 @@ function getRules() {
 
 function getPlugins(platform, env) {
     let plugins = [
+        new BundleAnalyzerPlugin({
+            analyzerMode: "static",
+            openAnalyzer: false,
+            generateStatsFile: true,
+            reportFilename: join(__dirname, "report", `${platform}-report.html`),
+            statsFilename: join(__dirname, "report", `${platform}-stats.json`),
+        }),
+
         new ExtractTextPlugin(mainSheet),
 
         // Vendor libs go to the vendor.js chunk
