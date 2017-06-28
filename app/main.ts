4a55ac9920cd35574adbaa28fe0b453dd3767651
Thomas Conner <thomas@kinvey.com>
2017-06-28 16:11:45 -0400

    Remove appkey and appsecret

--- app/main.ts
+++ app/main.ts
@@ -3,8 +3,8 @@ import { AppModule } from "./app.module";
 import { Kinvey } from 'kinvey-nativescript-sdk';
 
 Kinvey.init({
-  appKey: 'kid_B12-IEx7b',
-  appSecret: '13954d0eed70468baf999146e7a5508a'
+  appKey: '<app-key>',
+  appSecret: '<app-secret>'
 });
 
 platformNativeScriptDynamic().bootstrapModule(AppModule);
