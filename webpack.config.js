179954f0f382a965bb89d9155563d34366b5f96f
Panayot Cankov <panayot.cankov@telerik.com>
2017-10-13 14:03:54 +0300

    Update webpack to use raw-loader for css-es, use markingMode: none

--- webpack.config.js
+++ webpack.config.js
@@ -9,15 +9,14 @@ const ExtractTextPlugin = require("extract-text-webpack-plugin");
 const { BundleAnalyzerPlugin } = require("webpack-bundle-analyzer");
 const { AotPlugin } = require("@ngtools/webpack");
 
-function getPlatform(env) {
-    if (env.android) return "android";
-    if (env.ios) return "ios";
-    throw new Error("You need to provide a target platform!");
-}
-
 module.exports = env => {
+    function getPlatform() {
+        if (env.android) return "android";
+        if (env.ios) return "ios";
+        throw new Error("You need to provide a target platform!");
+    }
+
     const platform = getPlatform(env);
-    const plugins = getPlugins(platform, env);
     const config = {
         context: resolve("./app"),
         target,
@@ -64,11 +63,69 @@ module.exports = env => {
                 { test: /\.(ts)$/, use: [ "nativescript-dev-webpack/tns-aot-loader", "@ngtools/webpack", ] }
             ]
         },
-        plugins,
+        plugins: [
+            // Vendor libs go to the vendor.js chunk
+            new webpack.optimize.CommonsChunkPlugin({
+                name: ["vendor"],
+            }),
+    
+            // Define useful constants like TNS_WEBPACK
+            new webpack.DefinePlugin({
+                "global.TNS_WEBPACK": "true",
+            }),
+    
+            // Copy assets to out dir. Add your own globs as needed.
+            new CopyWebpackPlugin([
+                { from: "fonts/**" },
+                { from: "**/*.jpg" },
+                { from: "**/*.png" },
+                { from: "**/*.xml" },
+            ], { ignore: ["App_Resources/**"] }),
+    
+            // Generate a bundle starter script and activate it in package.json
+            new nsWebpack.GenerateBundleStarterPlugin([
+                "./vendor",
+                "./bundle",
+            ]),
+    
+            // Generate report files for bundles content
+            new BundleAnalyzerPlugin({
+                analyzerMode: "static",
+                openAnalyzer: false,
+                generateStatsFile: true,
+                reportFilename: join(__dirname, "report", `${platform}-report.html`),
+                statsFilename: join(__dirname, "report", `${platform}-stats.json`),
+            }),
+    
+            // Angular AOT compiler
+            new AotPlugin({
+                tsConfigPath: "tsconfig.aot.json",
+                entryModule: resolve(__dirname, "app/app.module#AppModule"),
+                typeChecking: false
+            }),
+    
+            // Resolve .ios.css and .android.css component stylesheets, and .ios.html and .android component views
+            new nsWebpack.UrlResolvePlugin({
+                platform: platform,
+                resolveStylesUrls: true,
+                resolveTemplateUrl: true
+            })
+        ]
     };
 
+    if (env.uglify) {
+        config.plugins.push(new webpack.LoaderOptionsPlugin({ minimize: true }));
+
+        // Work around an Android issue by setting compress = false
+        const compress = platform !== "android";
+        config.plugins.push(new webpack.optimize.UglifyJsPlugin({
+            mangle: { except: nsWebpack.uglifyMangleExcludes },
+            compress,
+        }));
+    }
+
     if (env.snapshot) {
-        plugins.push(new nsWebpack.NativeScriptSnapshotPlugin({
+        config.plugins.push(new nsWebpack.NativeScriptSnapshotPlugin({
             chunk: "vendor",
             projectRoot: __dirname,
             webpackConfig: config,
@@ -80,67 +137,3 @@ module.exports = env => {
 
     return config;
 };
-
-function getPlugins(platform, env) {
-    let plugins = [
-        // Vendor libs go to the vendor.js chunk
-        new webpack.optimize.CommonsChunkPlugin({
-            name: ["vendor"],
-        }),
-
-        // Define useful constants like TNS_WEBPACK
-        new webpack.DefinePlugin({
-            "global.TNS_WEBPACK": "true",
-        }),
-
-        // Copy assets to out dir. Add your own globs as needed.
-        new CopyWebpackPlugin([
-            { from: "fonts/**" },
-            { from: "**/*.jpg" },
-            { from: "**/*.png" },
-            { from: "**/*.xml" },
-        ], { ignore: ["App_Resources/**"] }),
-
-        // Generate a bundle starter script and activate it in package.json
-        new nsWebpack.GenerateBundleStarterPlugin([
-            "./vendor",
-            "./bundle",
-        ]),
-
-        // Generate report files for bundles content
-        new BundleAnalyzerPlugin({
-            analyzerMode: "static",
-            openAnalyzer: false,
-            generateStatsFile: true,
-            reportFilename: join(__dirname, "report", `${platform}-report.html`),
-            statsFilename: join(__dirname, "report", `${platform}-stats.json`),
-        }),
-
-        // Angular AOT compiler
-        new AotPlugin({
-            tsConfigPath: "tsconfig.aot.json",
-            entryModule: resolve(__dirname, "app/app.module#AppModule"),
-            typeChecking: false
-        }),
-
-        // Resolve .ios.css and .android.css component stylesheets, and .ios.html and .android component views
-        new nsWebpack.UrlResolvePlugin({
-            platform: platform,
-            resolveStylesUrls: true,
-            resolveTemplateUrl: true
-        })
-    ];
-
-    if (env.uglify) {
-        plugins.push(new webpack.LoaderOptionsPlugin({ minimize: true }));
-
-        // Work around an Android issue by setting compress = false
-        const compress = platform !== "android";
-        plugins.push(new webpack.optimize.UglifyJsPlugin({
-            mangle: { except: nsWebpack.uglifyMangleExcludes },
-            compress,
-        }));
-    }
-
-    return plugins;
-}
