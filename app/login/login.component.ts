efc4a626bb02860415587dded08da3298704382b
VladimirAmiorkov <vladimir.amiorkov@progress.com>
2019-05-29 17:27:10 +0300

    chore: remove 'read' from ViewChild calls

--- app/login/login.component.ts
+++ app/login/login.component.ts
@@ -20,12 +20,12 @@ export class LoginComponent implements OnInit {
   isLoggingIn = true;
   isAuthenticating = false;
 
-  @ViewChild("initialContainer", { read: ElementRef, static: false }) initialContainer: ElementRef;
-  @ViewChild("mainContainer", { read: ElementRef, static: false }) mainContainer: ElementRef;
-  @ViewChild("logoContainer", { read: ElementRef, static: false }) logoContainer: ElementRef;
-  @ViewChild("formControls", { read: ElementRef, static: false }) formControls: ElementRef;
-  @ViewChild("signUpStack", { read: ElementRef, static: false }) signUpStack: ElementRef;
-  @ViewChild("password", { read: ElementRef, static: false }) password: ElementRef;
+  @ViewChild("initialContainer", { static: false }) initialContainer: ElementRef;
+  @ViewChild("mainContainer", { static: false }) mainContainer: ElementRef;
+  @ViewChild("logoContainer", { static: false }) logoContainer: ElementRef;
+  @ViewChild("formControls", { static: false }) formControls: ElementRef;
+  @ViewChild("signUpStack", { static: false }) signUpStack: ElementRef;
+  @ViewChild("password", { static: false }) password: ElementRef;
 
   constructor(private router: Router,
     private userService: LoginService,
