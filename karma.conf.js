17ab9ffab51d3d3ddd409a925c1c1a69747cc949
SvetoslavTsenov <tsenov.svetoslav@gmail.com>
2019-07-24 23:34:48 +0300

    chore: update travis config

--- karma.conf.js
+++ karma.conf.js
@@ -1,6 +1,5 @@
-module.exports = function(config) {
-  config.set({
-    browserNoActivityTimeout: 40000,
+module.exports = function (config) {
+  const options = {
 
     // base path that will be used to resolve all patterns (eg. files, exclude)
     basePath: '',
@@ -12,9 +11,7 @@ module.exports = function(config) {
 
 
     // list of files / patterns to load in the browser
-    files: [
-      'app/**/*.js',
-    ],
+    files: ['app/tests/**/*.*'],
 
 
     // list of files to exclude
@@ -74,5 +71,38 @@ module.exports = function(config) {
     // Continuous Integration mode
     // if true, Karma captures browsers, runs the tests and exits
     singleRun: false
-  })
+  };
+
+  setWebpackPreprocessor(config, options);
+  setWebpack(config, options);
+
+  config.set(options);
+}
+
+function setWebpackPreprocessor(config, options) {
+  if (config && config.bundle) {
+    if (!options.preprocessors) {
+      options.preprocessors = {};
+    }
+
+    options.files.forEach(file => {
+      if (!options.preprocessors[file]) {
+        options.preprocessors[file] = [];
+      }
+      options.preprocessors[file].push('webpack');
+    });
+  }
+}
+
+function setWebpack(config, options) {
+  if (config && config.bundle) {
+    const env = {};
+    env[config.platform] = true;
+    env.sourceMap = config.debugBrk;
+    options.webpack = require('./webpack.config')(env);
+    delete options.webpack.entry;
+    delete options.webpack.output.libraryTarget;
+    const invalidPluginsForUnitTesting = ["GenerateBundleStarterPlugin", "GenerateNativeScriptEntryPointsPlugin"];
+    options.webpack.plugins = options.webpack.plugins.filter(p => !invalidPluginsForUnitTesting.includes(p.constructor.name));
+  }
 }
